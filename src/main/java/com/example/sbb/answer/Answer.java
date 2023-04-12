e7c3c8b956c84fdb8e0bed1f5ab525a1e102c518
Keon Ryong Cheong <heliotrop1024@gmail.com>
2023-04-12 12:12:26 +0900

    Sbb

---
+++ src/main/java/com/example/sbb/answer/Answer.java
@@ -0,0 +1,34 @@
+package com.example.sbb.answer;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import com.example.sbb.question.Question;
+import com.example.sbb.user.SiteUser;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
+
+    @ManyToOne
+    private Question question;
+
+    @ManyToOne
+    private SiteUser author;
+
+    @ManyToMany
+    Set<SiteUser> voter;
+}
---
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -0,0 +1,32 @@
+package com.example.sbb;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.sbb.answer.Answer;
+import com.example.sbb.answer.AnswerRepository;
+import com.example.sbb.question.Question;
+import com.example.sbb.question.QuestionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@SpringBootTest
+class SbbApplicationTests {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Transactional
+    @Test
+    void testJpa() {
+
+    }
+}
